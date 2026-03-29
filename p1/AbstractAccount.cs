b961c41f1c3ec0a23bddf7e6edb676a245527a64
GlebGCool <glebcool99@gmail.com>
2026-03-29 19:31:30 +0300

    Реализовал классы AbstractAccount и BankAccount; Сделал для них собственные исключения

---
+++ p1/AbstractAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p1
+{
+        enum Status{
+            Active,
+            Frozen
+        }
+    internal abstract class AbstractAccount
+    {
+
+
+        public Guid Id { get; protected set; }
+        public string Owner { get; protected set; }
+        protected decimal _balance;
+        public Status Status { get; protected set; }
+
+        public AbstractAccount (string owner, Status Status)
+        {
+            if (string.IsNullOrWhiteSpace(owner))
+                throw new ArgumentException("Владелец должен быть указан.");
+            this.Owner = owner;
+            this.Status = Status;
+        }
+
+        public abstract void Deposit(decimal amount);
+        public abstract void Withdraw(decimal amount);
+        public abstract string GetAccountInfo();
+
+
+
+    }
+}
---
+++ p1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            BankAccount account = new BankAccount("Pupa", Status.Active, Currency.RUB);
+            BankAccount frozenAccount = new BankAccount("Lupa", Status.Frozen, Currency.EUR);
+
+            
+            // Вывод информации об активном аккаунте
+            Console.WriteLine(account.GetAccountInfo());
+
+            // Пополнение баланса аккаунта
+            account.Deposit(1488);
+            Console.WriteLine(account.GetAccountInfo());
+            // Снятие средств с аккаунта
+            account.Withdraw(1337);
+            Console.WriteLine(account.GetAccountInfo());
+
+            // Вывод информации о замороженном аккаунте
+            Console.WriteLine(frozenAccount.GetAccountInfo());
+
+            // Попытка снаятия средств с замороженного аккаунта (прокидывается InvalidAccountOperationException)
+            frozenAccount.Withdraw(1488);
+
+        }
+    }
+}
